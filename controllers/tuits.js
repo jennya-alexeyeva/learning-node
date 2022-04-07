31983713244cb7b7dae0ec536d2da618b92e223b
jennya alexeyeva <jennyaalexeyeva@gmail.com>
2022-04-07 03:14:01 -0400

    minor cleanup

--- controllers/tuits.js
+++ controllers/tuits.js
@@ -4,13 +4,17 @@ export default [
     "postedBy": {"username": "NASA"},
     "tuit": "In 2021, our @NASAPersevere Mars rover landed",
     "likes": 345,
-    "dislikes": 89
+    "dislikes": 89,
+    "liked": false,
+    "disliked": false
   },
   {
     "_id": "234",
     "postedBy": { "username": "NASA" },
     "tuit": "Ingenuity helicopter takes flight.",
     "likes": 346785,
-    "dislikes": 84093432
+    "dislikes": 84093432,
+    "liked": false,
+    "disliked": false
   }
 ];
