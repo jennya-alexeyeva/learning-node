759ba810762176808460024bfc88e650697ac4cb
jennya alexeyeva <jennyaalexeyeva@gmail.com>
2022-04-15 22:19:27 -0400

    a9 stuff :))))))

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import helloController
   from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from "mongoose";
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 helloController(app);
 userController(app);
 tuitsController(app);
+mongoose.connect(process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev');
 app.listen(process.env.PORT || 4000);
